fe0b1a6e6949a36ed7eacfacabd948982b7b9df0
stack-guru <GoldenHand0302@gmail.com>
2025-05-19 13:42:20 -0600

    chore: update env value

--- tests/helpers/constants.ts
+++ tests/helpers/constants.ts
@@ -1,7 +1,7 @@
 import { CONTRACTS } from "@certusone/wormhole-sdk";
 import { PublicKey } from "@solana/web3.js";
 
-export const NETWORK = "DEVNET";
+export const NETWORK = "TESTNET";
 
 export const WORMHOLE_CONTRACTS = CONTRACTS[NETWORK];
 export const CORE_BRIDGE_PID = new PublicKey(WORMHOLE_CONTRACTS.solana.core);

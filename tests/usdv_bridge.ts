c1cd37b6b69b13ba793fea147af7f4297d5aebd0
stack-guru <GoldenHand0302@gmail.com>
2025-05-23 15:23:22 -0600

    feat: add foreign emitter

--- tests/usdv_bridge.ts
+++ tests/usdv_bridge.ts
@@ -11,11 +11,15 @@ import {
 import { assert, expect } from "chai";
 import { PublicKey, Connection, clusterApiUrl, Ed25519Program } from "@solana/web3.js";
 import { utils as solanaUtils } from '@wormhole-foundation/sdk-solana';
+import { chainToChainId } from "@wormhole-foundation/sdk";
 import { utils as solanaCoreUtils } from "@wormhole-foundation/sdk-solana-core";
 import { CORE_BRIDGE_PID } from "./helpers/constants";
 import { getPostedMessage } from "@certusone/wormhole-sdk/lib/cjs/solana/wormhole";
 import * as bridge from "../ts_sdk";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+const amoyAddress = process.env.AMOY_ADDRESS!;
 const EXISTING_MINT = new PublicKey("CUeFA3eTUcKCctTWuieMXLvn9ChAaMi5z6QhLRzJL3qn");
 const connection = new Connection(clusterApiUrl("devnet"), "processed");
 
@@ -26,14 +30,19 @@ describe("wormhole bridge", function () {
   anchor.setProvider(provider);
   const program = anchor.workspace.UsdvBridge as Program<UsdvBridge>;
 
-  // const realForeignEmitter = bridge.deriveForeignEmitterKey(WORMHOLE_PROGRAM_ID, chainToChainId("Ethereum"));
+  const realForeignEmitter = bridge.deriveForeignEmitterKey(program.programId, chainToChainId("PolygonSepolia"));
+  // const realForeignEmitterAddress = Buffer.alloc(32, amoyAddress, "hex");
+  const realForeignEmitterAddress = Buffer.from(
+    amoyAddress.toLowerCase().replace(/^0x/, "").padStart(64, "0"),
+    "hex"
+  );
   const realConfig = bridge.deriveConfigKey(program.programId);
 
   const wormholeCpi = solanaCoreUtils.getPostMessageCpiAccounts(
     program.programId,
     CORE_BRIDGE_PID,
     wallet.publicKey,
-    bridge.deriveWormholeMessageKey(program.programId, 2n) // sequence should be increased for every test
+    bridge.deriveWormholeMessageKey(program.programId, 20n) // sequence should be increased for every test
   );
 
   describe("Bridge", function () {
@@ -59,46 +68,74 @@ describe("wormhole bridge", function () {
     //   console.log('initialize trx = ', trx);
     // });
 
+    it("should get foreign emitter", async () => {
+      const { chain, address } =
+        await bridge.getForeignEmitterData(
+          program.programId,
+          chainToChainId("PolygonSepolia")
+        );
+
+      console.log('realForeignEmitterAddress = ', realForeignEmitterAddress);
+      expect(chain).equals(chainToChainId("PolygonSepolia"));
+      expect(address).deep.equals(realForeignEmitterAddress);
+    })
+
+    it("should register emitter", async () => {
+      // const realRegisterEmitterAccounts = {
+      //   owner: wallet.publicKey,
+      //   config: realConfig,
+      //   foreignEmitter: realForeignEmitter,
+      // }
+
+      // const trx = await program.methods
+      //   .registerEmitter(chainToChainId("PolygonSepolia"), [...realForeignEmitterAddress])
+      //   .accounts({ ...realRegisterEmitterAccounts })
+      //   .rpc();
+
+      // console.log('register emitter trx = ', trx);
+    })
+
     it("should send message", async () => {
-      const helloMessage = Buffer.from("All your base are belong to us");
+      // const helloMessage = Buffer.from("All your base are belong to us");
 
-      // save message count to grab posted message later
-      const sequence = (
-        await solanaCoreUtils.getProgramSequenceTracker(connection, program.programId, CORE_BRIDGE_PID)
-      ).value() + 1n;
-      console.log('sequence = ', sequence);
+      // // save message count to grab posted message later
+      // const sequence = (
+      //   await solanaCoreUtils.getProgramSequenceTracker(connection, program.programId, CORE_BRIDGE_PID)
+      // ).value() + 1n;
+      // console.log('sequence = ', sequence);
 
-      const trx = await program.methods
-        .sendMessage(helloMessage)
-        .accounts({
-          payer: wallet.publicKey,
-          config: realConfig,
-          wormholeProgram: CORE_BRIDGE_PID,
-          wormholeBridge: wormholeCpi.wormholeBridge,
-          wormholeFeeCollector: wormholeCpi.wormholeFeeCollector,
-          wormholeEmitter: wormholeCpi.wormholeEmitter,
-          wormholeSequence: wormholeCpi.wormholeSequence,
-          wormholeMessage: wormholeCpi.wormholeMessage,
-          clock: wormholeCpi.clock,
-          rent: wormholeCpi.rent,
-        })
-        .rpc();
+      // const trx = await program.methods
+      //   .sendMessage(helloMessage)
+      //   .accounts({
+      //     payer: wallet.publicKey,
+      //     config: realConfig,
+      //     wormholeProgram: CORE_BRIDGE_PID,
+      //     wormholeBridge: wormholeCpi.wormholeBridge,
+      //     wormholeFeeCollector: wormholeCpi.wormholeFeeCollector,
+      //     wormholeEmitter: wormholeCpi.wormholeEmitter,
+      //     wormholeSequence: wormholeCpi.wormholeSequence,
+      //     wormholeMessage: wormholeCpi.wormholeMessage,
+      //     clock: wormholeCpi.clock,
+      //     rent: wormholeCpi.rent,
+      //   })
+      //   .rpc();
 
-      console.log('send message trx = ', trx);
-      const { payload } =
-        (await getPostedMessage(
-          connection,
-          bridge.deriveWormholeMessageKey(program.programId, sequence)
-        )).message;
+      // console.log('send message trx = ', trx);
+      // const { payload } =
+      //   (await getPostedMessage(
+      //     connection,
+      //     bridge.deriveWormholeMessageKey(program.programId, sequence)
+      //   )).message;
 
-      expect(payload.readUint8(0)).equals(1); // payload ID
-      expect(payload.readUint16BE(1)).equals(helloMessage.length);
-      expect(payload.subarray(3)).deep.equals(helloMessage);
+      // expect(payload.readUint8(0)).equals(1); // payload ID
+      // expect(payload.readUint16BE(1)).equals(helloMessage.length);
+      // expect(payload.subarray(3)).deep.equals(helloMessage);
     });
 
     it("should receive message", async () => {
 
     })
+
   });
 
   // describe("usdv_bridge", () => {
